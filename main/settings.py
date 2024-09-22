8b42ac7ad9033ad5909e9bc404c08cb1f75a5c6d
Forhad <71933188+Forhad700@users.noreply.github.com>
2024-09-22 17:19:03 +0600

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/','static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'build/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
